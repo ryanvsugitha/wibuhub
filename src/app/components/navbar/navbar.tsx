7dcb588e0529db534635fd29c6354363140c24d1
Ryan Vijaya Sugitha <ryansugitha@gmail.com>
2024-10-03 13:26:12 +0700

    update

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Logo from "@/app/assets/wibuhub_logo.svg";
 import Link from "next/link";
 
-export default async function NavBar() {
+export default function NavBar() {
   return (
     <div className="bg-cyan-600 flex flex-row items-center justify-between p-1">
       <div className="flex flex-row items-center gap-3">
@@ -16,10 +16,9 @@ export default async function NavBar() {
           />
         </Link>
         <div>Seasonal Anime</div>
-        <div>Top Anime</div>
       </div>
       <div>
-        profile
+        <Link href={"../login"}>Login</Link>
       </div>
     </div>
   );
---
+++ src/app/login/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button, Paper, PasswordInput, Text, TextInput } from "@mantine/core";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/app/assets/wibuhub_logo.svg";
+
+export default function Login() {
+  return (
+    <> 
+      <div className="flex justify-center items-center min-h-screen w-full">
+        <Paper shadow="sm" withBorder p="xl" className="w-1/4">
+          <div className="flex flex-col gap-3 items-center ">
+            <Image
+              className="object-contain"
+              src={Logo}
+              width={120}
+              height={100}
+              alt=""
+            />
+            <Text>Login</Text>
+            <TextInput label="Username or email" error="" className="w-full"/>
+            <PasswordInput label="Password" error="" className="w-full"/>
+            <Link href={"../register"} className="w-full">
+              <Text size="xs" c="red" ta="right">
+                Don't have an account? Register instead
+              </Text>
+            </Link>
+            <Button variant="filled" color="cyan">
+              Login
+            </Button>
+          </div>
+        </Paper>
+      </div>
+    </>
+  );
+}
